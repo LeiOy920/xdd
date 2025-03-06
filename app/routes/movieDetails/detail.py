f9f173aa73e9347408c84a65cf8d135bc19193db
欧阳 <2697685938@qq.com>
2025-03-06 18:14:07 +0800

    完成详情页，minio访问成功

--- app/routes/movieDetails/detail.py
+++ app/routes/movieDetails/detail.py
@@ -2,10 +2,11 @@ import base64
 
 import requests
 from flask import Blueprint, request
-from sqlalchemy import or_
 
+from app.config import minio_storage
 from app.models.moviedetail import MovieDetail
 from app.models.sentiment_score import SentimentScore
+
 
 detail = Blueprint('detail', __name__)
 
@@ -15,22 +16,43 @@ def getMovieDetails():
     id = request.args.get('id')
     movie_name = request.args.get('movie_name')
 
-
-
     if id is not None:
         movie_basic = MovieDetail.query.filter_by(id=id).first()
     else:
-        movie_basic = MovieDetail.query.filter(or_(movie_name == movie_name)).first()
+        print(movie_name)
+        movie_basic = MovieDetail.query.filter(MovieDetail.movie_name == movie_name).first()
 
     if movie_basic is None:
         return {'msg': '电影不存在'}
 
-    sentiment_score = SentimentScore.query.filter_by(m_id=movie_basic.id)
+    sentiment_score = SentimentScore.query.filter_by(m_id=movie_basic.id).first()
+
+    return {
+            'id': movie_basic.id,
+            'movie_name': movie_basic.movie_name,
+            'movie_image': movie_basic.movie_image,
+            'director': movie_basic.director,
+            'screenwriter': movie_basic.screenwriter,
+            'release_date': movie_basic.release_date,
+            'runtime': movie_basic.runtime,
+            'production_country_region': movie_basic.production_country_region,
+            'douban_rating': movie_basic.douban_rating,
+            'also_known_as': movie_basic.also_known_as,
+            'genre': movie_basic.genre.split(),
+            'starring': movie_basic.staring.split() if movie_basic.starring else [],
+            'ciyun_image': 'b.png',
+            'sentiment_score': {'average_score': sentiment_score.average_score,
+                              'total_references': sentiment_score.total_references,
+                              'very_like': sentiment_score.very_like,
+                              's_like': sentiment_score.s_like,
+                              'normal': sentiment_score.normal,
+                              'dislike': sentiment_score.dislike,
+                              'very_dislike': sentiment_score.very_dislike}
+        }
 
 
 
 
-    
 
 
 
@@ -46,3 +68,12 @@ def download_image():
     return {'image': image_base64}
 
 
+@detail.route('/getWordCloud', methods=['GET'])
+def getWordCloud():
+    id = request.args.get('id')
+    file = minio_storage.get_file(object_name=f'word_cloud{id}.png', bucket_name='movie-wordclouds')
+    if file is not None:
+        image_base64 = base64.b64encode(file.read()).decode('utf-8')
+    else:
+        image_base64 = ''
+    return {'image': image_base64}
--- app/routes/movieDetails/dm.py
+++ app/routes/movieDetails/dm.py
@@ -1,20 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
+from app.utils.minIOUtils import MinIOStorage
 
 dm = Blueprint('dm', __name__)
 
+from app.config import minio_storage
 
 @dm.route('/', methods=['POST'])
 def index():
     try:
-        with open('./static/comments1.txt', 'r', encoding='utf-8') as file:
-            lines = file.readlines()
+        id = request.json.get('id')
+        file = minio_storage.get_file(object_name=f'comments{id}.txt', bucket_name = 'movie-reviews')  # 包含文件内容的BytesIO对象，或None（如果文件不存在）
+        if file is not None:
+            lines = [line.decode('utf-8') for line in file.readlines()]
             # 提取偶数行内容（索引从 0 开始，所以索引为奇数的行是偶数行）
             even_lines = [line.strip() for i, line in enumerate(lines) if i % 2 != 0]
             # 将偶数行内容合并成一个字符串
             text = '_,_'.join(even_lines)
             words = text.split('_,_')  # 按行分割
-    except FileNotFoundError:
+        else:
+            words = []
+    except (FileNotFoundError, AttributeError):
         words = []
     return {'data': words}
 
